8f2ed77b765b637b1a22fe5fedc36aabb9bce720
GrilldFishTaco <aidan.chatterji@gmail.com>
2024-07-11 15:12:29 -0400

    testing2

---
+++ bankAccount.py
@@ -0,0 +1,9 @@
+class bankaccount:
+    def __init__(self, name, pin):
+        # Set initial values
+        self.balance = 0
+        self.name = name
+        self.pin = pin
+
+    def withdraw(amount):
+        
---
+++ test_bankAccount.py
@@ -0,0 +1,4 @@
+class test:
+    def setup(self):
+
+    def test_name(self):
